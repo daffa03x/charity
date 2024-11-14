5cf66aa1cb1f113d61038f932c0a44a5c34f57a8
daffa03x <mobadaff@gmail.com>
2024-11-14 14:18:43 +0700

    materi 2.8 komposisi komponent dengan slot

---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports = {
+  charities: [
+    {
+      id: 1,
+      title: "First Charity Project",
+      category: "Money",
+      thumbnail: "https://via.placeholder.com/150",
+      pledged: 0,
+      target: 100000,
+      date_end: +new Date("20 November 2024"),
+      profile_photo: "https://via.placeholder.com/150",
+      profile_name: "John Doe",
+      no_pledgedes: 0,
+    },
+  ],
+};
---
+++ src/pages/Home.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Header from "../components/Header.svelte";
+    import Welcome from "../components/Welcome.svelte";
+    import Footer from "../components/Footer.svelte";
+    import CharityList from "../components/CharityList.svelte";
+    import Promo from "../components/Promo.svelte";
+    import charities from "../data/charities.js";
+</script>
+
+    <Header />
+    <Welcome />
+    <CharityList charities={charities}/>
+    <Promo />
+    <Footer />
